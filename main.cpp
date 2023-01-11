82fa039b888fd198c5ba086fd09cfc00a8e95f91
jempiere <jaysonbunnell@gmail.com>
2023-01-10 18:41:50 -0700

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//main.cpp
+#include <iostream>
+
+//print macro because the std::cout can handle strings and integers
+#define print(a) std::cout << a << std::endl
+
+int main(){
+  print("Hello, World!");
+  return 0;
+}
